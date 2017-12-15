72b1d963aff97b1f3cd2e1b87240235e57d96636
maruian@floridauniversitaria.es <maruian@floridauniversitaria.es>
2017-12-15 19:37:04 +0100

    Initial commit

---
+++ app/src/main/java/com/example/a2dam/provadrive/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.a2dam.provadrive;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.drive.Drive;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    GoogleSignInClient mGoogleSignInClient;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mGoogleSignInClient = buildGoogleSignInClient();
+        Toast.makeText(this.getApplicationContext(),"Google Signin obtenido",Toast.LENGTH_SHORT).show();
+    }
+
+    private GoogleSignInClient buildGoogleSignInClient() {
+        GoogleSignInOptions signInOptions =
+                new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestScopes(Drive.SCOPE_FILE)
+                        .build();
+        return GoogleSignIn.getClient(this, signInOptions);
+    }
+}
